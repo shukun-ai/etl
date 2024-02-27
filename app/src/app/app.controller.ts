da5e6fe17cca1a9e6a4f11e955bda3698d3d6de5
Chase Wu <minowu@foxmail.com>
2024-02-27 11:19:43 +0800

    feat: remove unused files

--- app/src/app/app.controller.ts
+++ app/src/app/app.controller.ts
@@ -1,45 +1,9 @@
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  Param,
-  Query,
-} from '@nestjs/common';
-
-import { AppService } from './app.service';
-
-import { appSchema } from './app.schema';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get('/db/:dbName/collection/:collectionName')
-  async getData(
-    @Param('dbName') dbName: string,
-    @Param('collectionName') collectionName: string,
-    @Query()
-    queries: {
-      limit: string;
-      offset: string;
-      minUpdatedAt: string;
-      maxUpdatedAt: string;
-    }
-  ) {
-    const parsedQueries = this.getQueries(queries);
-    const rows = await this.appService.getData(
-      { dbName, collectionName },
-      parsedQueries
-    );
-    return rows;
-  }
-
-  getQueries(queries: unknown) {
-    try {
-      const parsedQueries = appSchema.parse(queries);
-      return parsedQueries;
-    } catch (error) {
-      throw new BadRequestException(error);
-    }
+  @Get()
+  async getData() {
+    return 'hello world';
   }
 }
